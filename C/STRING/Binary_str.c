70507e2a964f7a7bbc46fd59bf92699bdca5d2e5
its-Kumar <49340432+its-Kumar@users.noreply.github.com>
2019-10-30 11:37:43 +0530

    Add files via upload

---
+++ C/STRING/Binary_str.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+
+int is_binary(char *s){
+    int l = strlen(s);
+    int len =0;
+    while(*s){
+        if(*s=='1' || *s=='0')
+            len++;
+        s++;
+    }
+    if(len == l-1)
+        return 1;
+    return 0;
+}
+
+int main(void){
+    char str[20];
+    fgets(str,20,stdin);
+    puts(str);
+    if(is_binary(str))
+        printf("Binary String...\n");
+    else
+        printf("Not a binary string...\n");
+return 0;
+}
